7b3c883ad24bc6ec135c242ecaffe81269f0c549
Ayan Nadaf <nadafayan95@gmail.com>
2025-03-13 17:15:40 +0530

    Fixed Hero Image issue

--- client/src/Components/HeroSection.jsx
+++ client/src/Components/HeroSection.jsx
@@ -5,7 +5,7 @@ const HeroSection=()=>{
     return(
         <>
         <div className="container">
-            <div className="grid grid-cols-1 md:grid-cols-2 min-h-[550px] w-[100%] ">
+            <div className="grid grid-cols-1 md:grid-cols-2 min-h-[550px] w-screen ">
                 <div className=" space-y-5 flex flex-col justify-center items-center text-center md:text-left py-20 px-20 md:py-0 md:px-0  md:items-start md:ml-20 w-full">
                     <h1 className=" text-4xl xl:text-5xl font-bold ">
                         Earn Extra Cash with Local Tasks
@@ -18,8 +18,8 @@ const HeroSection=()=>{
                     </div>
                 </div>
 
-                <div className="flex justify-center items-center ml-32 mr-5">
-                    <img src={logo} alt="" className=" w-[90%] md:w-[700px] xl:w-[1100px] h-[400px] rounded-xl shadow-2xl" />
+                <div className="flex justify-center items-center md:ml-0 overflow-hidden">
+                    <img src={logo} alt="People working" className="md:w-[90%] xl:w-[80%] w-full h-auto max-w-full rounded-xl shadow-2xl object-cover" />
                 </div>
             </div>
         </div>
