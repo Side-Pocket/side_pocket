919a8f90cbe335e6039eeffa7b4a2d0ff263eef3
NiketNannavare <niketnannaware703@gmail.com>
2025-03-26 11:36:42 +0530

    deployment issues resolve 1

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@
 // import axios from "axios";
 
 import Header from "./components/Header";
-import Footer from './components/Footer';
+// import Footer from './components/Footer';
 // import { Router, Route, Routes } from "react-router";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Signup from "./components/Signup";
@@ -59,7 +59,7 @@ function App() {
         <Route path="/about" element={<About />} />
       </Routes>
     
-      <Footer />
+      {/* <Footer /> */}
     </Router>
 
     </div>
